e685d3fa00a0ed1b653e39d5627df81eb3daf874
Caroline Chan <carolinechan304@gmail.com>
2022-10-19 15:07:31 +0700

    Add computed property to identify player answer, add function to restart the game, add logic to the submit button

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,11 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
-    var pick: [String] = ["Rock", "Paper", "Scissors"].shuffled()
-    @State private var winCondition: Bool = true
     let items: [String] = ["Rock", "Paper", "Scissors"]
+    @State private var initialPick: String = "Paper"
+    @State private var winCondition: Bool = true
     @State private var userPick: String = "Paper"
     @State private var score: Int = 0
+    @State private var round: Int = 1
+    @State private var showAlert: Bool = false
+    
+    private var isCorrect: Bool {
+        if winCondition {
+            if initialPick == "Rock" {
+                switch userPick {
+                case "Paper":
+                    return true
+                default:
+                    return false
+                }
+            } else if initialPick == "Paper" {
+                switch userPick {
+                case "Scissors":
+                    return true
+                default:
+                    return false
+                }
+            } else {
+                switch userPick {
+                case "Rock":
+                    return true
+                default:
+                    return false
+                }
+            }
+        } else {
+            if initialPick == "Rock" {
+                switch userPick {
+                case "Scissors":
+                    return true
+                default:
+                    return false
+                }
+            } else if initialPick == "Paper" {
+                switch userPick {
+                case "Rock":
+                    return true
+                default:
+                    return false
+                }
+            } else {
+                switch userPick {
+                case "Paper":
+                    return true
+                default:
+                    return false
+                }
+            }
+        }
+    }
     
     var body: some View {
         VStack {
@@ -22,7 +74,7 @@ struct ContentView: View {
             
             Form {
                 Section {
-                    Text("App initial pick is \(pick[0])")
+                    Text("App initial pick is \(initialPick)")
                     Text(winCondition ? "Player must win" : "Player must lose")
                 } header: {
                     Label("Initial condition", systemImage: "gamecontroller")
@@ -42,10 +94,23 @@ struct ContentView: View {
             }
             
             Button{
-                
+                if isCorrect {
+                    score += 10
+                }
+                initialPick = items[Int.random(in: 0...2)]
+                winCondition.toggle()
+                if round == 10 {
+                    showAlert = true
+                }
+                round += 1
             } label: {
                 Label("SUBMIT", systemImage: "paperplane")
                     .foregroundColor(.red)
+            }
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Game Over"), message: Text("Your score is \(score)"), dismissButton: .default(Text("Restart")){
+                    restart()
+                })
             }
         }
     }
@@ -54,5 +119,12 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension ContentView {
+    func restart() {
+        round = 0
+        score = 0
     }
 }
