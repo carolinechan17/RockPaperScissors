537d727080e23241b9043643f2d4ef2530cbd9f7
Caroline Chan <carolinechan304@gmail.com>
2022-10-19 14:38:59 +0700

    Add form to show initial condition and pick, add text to show score, add button to submit answer

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    var pick: [String] = ["Rock", "Paper", "Scissors"].shuffled()
+    @State private var winCondition: Bool = true
+    let items: [String] = ["Rock", "Paper", "Scissors"]
+    @State private var userPick: String = "Paper"
+    @State private var score: Int = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("SCORE: \(score)")
+                .font(.title2.weight(.bold))
+                .foregroundColor(.red)
+            
+            Form {
+                Section {
+                    Text("App initial pick is \(pick[0])")
+                    Text(winCondition ? "Player must win" : "Player must lose")
+                } header: {
+                    Label("Initial condition", systemImage: "gamecontroller")
+                        .foregroundColor(.red)
+                }
+                
+                Section {
+                    Picker("Player pick", selection: $userPick) {
+                        ForEach(items, id: \.self) { item in
+                            Text(item)
+                        }
+                    }
+                } header: {
+                    Label("Pick to satisfy condition", systemImage: "checkmark")
+                        .foregroundColor(.red)
+                }
+            }
+            
+            Button{
+                
+            } label: {
+                Label("SUBMIT", systemImage: "paperplane")
+                    .foregroundColor(.red)
+            }
         }
-        .padding()
     }
 }
 
